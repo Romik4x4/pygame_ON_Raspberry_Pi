00501aa92807a327397a91faad193eb1b18a97ac
Romik4x4 <r.k@pajero4x4.ru>
2016-05-13 15:08:22 +0300

    sin

--- sin.py
+++ sin.py
@@ -4,114 +4,46 @@ import os
 import pygame
 import time
 import random
+import math
 
 os.putenv('SDL_FBDEV', '/dev/fb1')
 os.putenv('SDL_MOUSEDRV', 'TSLIB')
 os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
 
+pygame.init()
+pygame.mouse.set_visible(False)
 
-class pyscope :
-    screen = None;
-    
-    def __init__(self):
-        "Ininitializes a new pygame screen using the framebuffer"
-        # Based on "Python GUI in Linux frame buffer"
-        # http://www.karoltomala.com/blog/?p=679
-        disp_no = os.getenv("DISPLAY")
-        if disp_no:
-            print "I'm running under X display = {0}".format(disp_no)
-        
-        # Check which frame buffer drivers are available
-        # Start with fbcon since directfb hangs with composite output
-        drivers = ['fbcon', 'directfb', 'svgalib']
-        found = False
-        for driver in drivers:
-            # Make sure that SDL_VIDEODRIVER is set
-            if not os.getenv('SDL_VIDEODRIVER'):
-                os.putenv('SDL_VIDEODRIVER', driver)
-            try:
-                pygame.display.init()
-            except pygame.error:
-                print 'Driver: {0} failed.'.format(driver)
-                continue
-            found = True
-            break
-    
-        if not found:
-            raise Exception('No suitable video driver found!')
-        
-        size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-        print "Framebuffer size: %d x %d" % (size[0], size[1])
-        self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
-        # Clear the screen to start
-        self.screen.fill((0, 0, 0))        
-        # Initialise font support
-        pygame.font.init()
-        # Render the screen
-        pygame.display.update()
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+adcColor = (255, 255, 0)
+PI = 3.141592653
 
-    def __del__(self):
-        "Destructor to make sure pygame shuts down, etc."
-    
-    def drawGraticule(self):
-        "Renders an empty graticule"
-        # The graticule is divided into 10 columns x 8 rows
-        # Each cell is 50x40 pixels large, with 5 subdivisions per
-        # cell, meaning 10x8 pixels each.  Subdivision lines are
-        # displayed on the central X and Y axis
-        # Active area = 10,30 to 510,350 (500x320 pixels)
-        borderColor = (255, 255, 255)
-        lineColor = (64, 64, 64)
-        subDividerColor = (128, 128, 128)
-        # Outer border: 2 pixels wide
-        pygame.draw.rect(self.screen, borderColor, (8,28,312,212), 2)
-        # Horizontal lines (40 pixels apart)
-        for i in range(0, 7):
-            y = 70+i*40
-            pygame.draw.line(self.screen, lineColor, (10, y), (310, y))
-        # Vertical lines (50 pixels apart)
-        for i in range(0, 9):
-            x = 60+i*50
-            pygame.draw.line(self.screen, lineColor, (x, 30), (x, 210))
-        # Vertical sub-divisions (8 pixels apart)
-        for i in range(1, 40):
-            y = 30+i*8
-            pygame.draw.line(self.screen, subDividerColor, (312, y), (312, y))
-        # Horizontal sub-divisions (10 pixels apart)
-        for i in range(1, 50):
-            x = 10+i*10
-            pygame.draw.line(self.screen, subDividerColor, (x, 212), (x, 212))
+size = (320,240)
+screen = pygame.display.set_mode(size)
+borderColor = (255, 255, 255)
+lineColor = (64, 64, 64)
+subDividerColor = (128, 128, 128)
+pygame.draw.rect(screen, borderColor, (8,28,312,212), 2)
 
-    def test(self):
-        "Test method to make sure the display is configured correctly"
-        adcColor = (255, 255, 0)  # Yellow
-        self.drawGraticule()
-        # Render the Adafruit logo at 10,240
-      #  logo = pygame.image.load('adafruit_logo.gif').convert()
-      #  self.screen.blit(logo, (10, 335))
-        # Get a font and use it render some text on a Surface.
-      #  font = pygame.font.Font(None, 30)
-      #  text_surface = font.render('pyScope (%s)' % "0.1", 
-      #      True, (255, 255, 255))  # White text
-        # Blit the text at 10, 0
-      #  self.screen.blit(text_surface, (10, 0))
-        # Render some text with a background color
-      #  text_surface = font.render('Channel 0',
-      #      True, (0, 0, 0), (255, 255, 0)) # Black text with yellow BG
-        # Blit the text
-      #  self.screen.blit(text_surface, (540, 30))
-        # Update the display
-        pygame.display.update()
-        # Random adc data
-        yLast = 260
-        for x in range(10, 509):
-            y = random.randrange(30, 350, 2) # Even number from 30 to 350
-            pygame.draw.line(self.screen, adcColor, (x, yLast), (x+1, y))
-            yLast = y
-            pygame.display.update()
+def mymap(x,in_min,in_max,out_min,out_max):
+	return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
-# Create an instance of the PyScope class
-scope = pyscope()
-scope.test()
-# Wait 10 seconds
-time.sleep(10)
+MainLoop = True
+
+while MainLoop:
+	yLast = 238
+	for x in range(3,317):
+		y = int(math.sin(x))
+		y = mymap(y,0,1,3,237)
+		print "%d %d\r\n" % (x,y)
+		pygame.draw.line(screen, adcColor, (x, yLast), (x+1, y))
+		yLast = y
+		pygame.display.update()
+	for event in pygame.event.get():
+		pos = pygame.mouse.get_pos()
+		MainLoop = False
+
+pygame.quit()
